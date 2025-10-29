c9cfed131922d415cf2c35d3a67ca86f25b7eef0
jaimergp <jaimergp@users.noreply.github.com>
2025-10-29 22:56:15 +0100

    create some copies/aliases in windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -25,3 +25,15 @@ if errorlevel 1 exit 1
 
 cmake --build . --target install --config Release
 if errorlevel 1 exit 1
+
+if %ZLIB_COMPAT%==1 (
+      :: Some OSS libraries are happier if z.lib exists, even though it's not typical on Windows.
+      copy %LIBRARY_LIB%\zlib.lib %LIBRARY_LIB%\z.lib || exit 1
+
+      :: Qt in particular goes looking for this one (as of 4.8.7).
+      copy %LIBRARY_LIB%\zlib.lib %LIBRARY_LIB%\zdll.lib || exit 1
+
+      :: python>=3.10 depend on this being at %PREFIX%
+      copy %LIBRARY_BIN%\zlib1.dll %LIBRARY_BIN%\zlib.dll || exit 1
+      copy %LIBRARY_BIN%\zlib1.dll %PREFIX%\zlib.dll || exit 1
+)
